c2939ba25c3d6f0bd8cb7545699f0c4874357376
H0v <45029523+H0v@users.noreply.github.com>
2022-04-28 13:21:07 +0400

    https://habr.com/ru/post/483202/

--- routes/Products.js
+++ routes/Products.js
@@ -5,69 +5,70 @@ const productModel = require('../models/Products');
 
 const { formatPrice } = require('../helpers/priceFormat');
 
-router.get("/get-all", async (req, res) => {
+router.get("/product", async (req, res) => {
     try {
         const products = await productModel.find();
-        res.json(products);
+        res.json({ products });
     }
     catch(e) {
         res.json({message: `Error: Something went wrong.`});
     }
 });
 
-router.post("/create-product", async (req, res) => {
+router.post("/product", async (req, res) => {
    try {
-       const { body } = req;
+       const { image, title, description, price } = req.body;
        const Product = new productModel({
-           image: body.image,
-           title: body.title,
-           description: body.description,
-           price: formatPrice(parseInt(body.price)),
+           image,
+           title,
+           description,
+           price: formatPrice(parseInt(price)),
        });
        await Product.save();
-       res.json({message: 'Product successfully added...'});
+       res.json({ success: true });
    }
    catch(e) {
-       res.json({message: `Something went wrong while adding product... ${e}`})
+       res.json({ message: `Something went wrong while adding product... ${e}` })
    }
 });
 
-router.get('/:id', async (req, res) => {
+router.get('/product/:id', async (req, res) => {
     try {
-        const {params} = req;
-        const product = await productModel.findById(params.id).select('- __v')
-        res.json({product});
+        const { id } = req.params;
+        const product = await productModel.findById(id).select('- __v')
+        res.json({ product });
     } catch (e) {
-        res.json({message: "Something went wrong while trying to get single user."});
+        res.json({ message: "Something went wrong while trying to get single user." });
     }
 });
 
-router.put('/update-product', async (req, res) => {
+router.put('/product/:id', async (req, res) => {
     try {
-        const { body } = req;
+        const { id } = req.params;
+        const { title, description, price } = req.body;
         await productModel.findOneAndUpdate(
-            {_id: body.id},
+            { _id: id },
             {
-                title: body.title,
-                description: body.description,
-                price: formatPrice(body.price),
+                title,
+                description,
+                price: formatPrice(price),
             }
         );
-        res.json({message: 'Product successfully updated...'});
+        res.json({ success: true });
     }
     catch(e) {
         res.json({message: 'Something went wrong while updating product...'});
     }
 });
 
-router.delete('/delete-product', async (req, res) => {
+router.delete('/product/:id', async (req, res) => {
    try {
-       const { body } = req;
-       await productModel.findOneAndDelete({_id: body.id});
-       res.json({message: 'Product deleted successfully.'});
+       const { id } = req.params;
+       await productModel.findOneAndDelete({ _id: id });
+       res.json({ success: true });
    }
    catch(e) {
-       res.json({message: 'Something went wrong while deleting product.'});
+       res.json({ message: 'Something went wrong while deleting product.' });
    }
 });
 
